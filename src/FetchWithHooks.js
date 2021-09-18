b6c751a044e851d40a51eecca5fdaaf998e0d00c
Fredi Anriko <fredianriko@gmail.com>
2021-09-19 01:31:20 +0700

    making the useEffect fetching dependand to the search button

--- src/FetchWithHooks.js
+++ src/FetchWithHooks.js
@@ -4,19 +4,25 @@ import axios from "axios";
 function FetchWithHooks() {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("Redux");
+  const [search, setSearch] = useState("Redux");
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(`https://hn.algolia.com/api/v1/search?query=${query}`);
+      const result = await axios(`https://hn.algolia.com/api/v1/search?query=${search}`);
       setData(result.data);
       console.log(result);
     };
     fetchData();
-  }, [query]);
+  }, [search]);
+
+  //trigger fetching whenever there are changes to the input form, but start fetching when the search button is clicked
 
   return (
     <>
       <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
       <ul>
         {data.hits.map((item) => (
           <li key={item.objectID}>
